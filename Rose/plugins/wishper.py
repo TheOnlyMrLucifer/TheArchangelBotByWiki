2bb4a21cb8af6ed167a51e401f7f06460b9cf7ae
Chloe Decker <69217587+TheOnlyMrLucifer@users.noreply.github.com>
2022-11-15 10:01:58 +0100

    Update wishper.py

--- Rose/plugins/wishper.py
+++ Rose/plugins/wishper.py
@@ -20,11 +20,11 @@ async def wishper_ai(_, sz: InlineQuery):
             or (query.startswith('@') and len(split) == 1):
         title = f"🔐 Write a whisper message"
         content = ("**Send whisper messages through inline mode**\n\n"
-                   "Usage: `@TheArchangelBot [@username] text`")
-        description = "Usage: @TheArchangelBot [@username] text"
+                   "Usage: `@BadBSiBot[@username] text`")
+        description = "Usage: @BadBSiBot [@username] text"
         button = InlineKeyboardButton(
             "More-Help",
-            url="https://t.me/TheArchangelBot?start=learn"
+            url="https://t.me/BadBSiBot?start=learn"
         )
 
     elif not query.startswith('@'):
@@ -72,11 +72,11 @@ __HELP__ = """
 the other way to use me is to write the inline query by your self
 the format should be in this arrangement
 
-@TheArchangelBot your whisper @username
+@BadBSiBot your whisper @username
 
 now I'll split out the format in 3 parts and explain every part of it
 
-1- @TheArchangelBot
+1- @BadBSiBot
 this is my username it should be at the beginning of the inline query so I'll know that you are using me and not another bot.
 
 2- whisper message
@@ -86,7 +86,7 @@ it is the whisper that will be sent to the target user, you need to remove your 
 you should replace this with target's username so the bot will know that the user with this username can see your whisper message.
 
 example:- 
-@TheArchangelBot hello this is a test @TheOnlyMrLucifer
+@BadBSiBot hello this is a test
 
 📎 the bot works in groups and the target user should be in the same group with you
 what you are waiting for?!
